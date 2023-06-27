2f2ac2c045b3557fd3edbaf27f097c0a05fb0794
HangNT169 <HangNT169@fpt.edu.vn>
2023-06-27 13:07:12 +0700

    B1_Hello world

--- src/B1_Overview/HelloWorld.java
+++ src/B1_Overview/HelloWorld.java
@@ -2,19 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package mob1014_mob1014.pkg03_block2_su23;
+package B1_Overview;
 
 /**
  *
  * @author hangnt
  */
-public class MOB1014_MOB101403_BLOCK2_SU23 {
+public class HelloWorld {
 
     /**
      * @param args the command line arguments
      */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//        // In => C => printf
+//        // Java => sout
+//        System.out.println("Hihi");
+//        // C => Xuong dong \n 
+//        System.out.println("hihihih1");
+//    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
